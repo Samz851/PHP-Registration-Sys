ce5bc53de61dce1a871168689aabfe195264f3c5
Samz851 <sam.otb@hotmail.ca>
2018-04-17 01:15:19 -0400

    links configuration

--- views/header.php
+++ views/header.php
@@ -87,7 +87,7 @@
         
 		<!-- Website Font style -->
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-		<link rel="stylesheet" href="/php-repos/registration/style.css">
+		<link rel="stylesheet" href="https://php-registrtion-system.herokuapp.com/style.css">
 		<!-- Google Fonts -->
 		<link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
@@ -95,17 +95,17 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="http://localhost/php-repos/registration/">PHP-USER-SYS</a>
+            <a class="navbar-brand" href="https://php-registrtion-system.herokuapp.com/">PHP-USER-SYS</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarColor01">
                 <ul class="navbar-nav mr-auto">
-                <li class="nav-item active"><a class="nav-link" href='/php-repos/registration/views/home.php'>Home <span class="sr-only">(current)</span></a></li>
-                <li class="nav-item"><a class="nav-link" href='/php-repos/registration/views/form.php'>Registration</a></li>
+                <li class="nav-item active"><a class="nav-link" href='https://php-registrtion-system.herokuapp.com/views/home.php'>Home <span class="sr-only">(current)</span></a></li>
+                <li class="nav-item"><a class="nav-link" href='https://php-registrtion-system.herokuapp.com/views/form.php'>Registration</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">About</a></li>
-                <li class="nav-item"><a class="nav-link" href='/php-repos/registration/views/login.php'>Login</a></li>
+                <li class="nav-item"><a class="nav-link" href='https://php-registrtion-system.herokuapp.com/views/login.php'>Login</a></li>
                 <li class="nav-item"><a class="nav-link" href='?logout'>Logout</a></li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
